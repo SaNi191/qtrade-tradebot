bc0681d546d3eb658cee63c5bcd7bee24c68ebf1
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-06-01 12:13:26 -0600

    worked on TokenManager system

--- qtrade-tradebot/main.py
+++ qtrade-tradebot/main.py
@@ -1,5 +1,38 @@
 from env_vars import ACCESS_TOKEN
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+
+# creating SQLAlchemy engine and session connecting to bot.db
+engine = create_engine('sqlite:///bot.db', echo = True)
+
+session = sessionmaker(bind = engine)
+
+from models import Base, Tokens
+Base.metadata.create_all(engine)
+
+
+
+class TokenManager():
+    def __init__(self, session):
+        # session will be used to obtain refresh and access tokens
+        self.session = session
+        self.get_refresh_token()
+        self.get_access_token()
+    
+    def get_refresh_token(self):
+        with self.session() as session:
+            result = session.select(Tokens).where(Tokens.id == 1)
+            if result is None:
+                # no refresh token in database: grab from environment variable (likely expired)
+                # will need to check for expiry 
+                from env_vars import BOT_TOKEN
+                base_token = Tokens()
+
+
+
+    def get_access_token(self):
+        pass
 
 
 
--- qtrade-tradebot/models.py
+++ qtrade-tradebot/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.types import LargeBinary, TypeDecorator
 from sqlalchemy import DateTime
 from cryptography.fernet import Fernet
 from env_vars import ENCRYPTION_KEY
-
+import datetime
 
 # models for bot database:
 
@@ -25,19 +25,20 @@ class EncryptedToken(TypeDecorator):
 
     def __init__(self, key: bytes):
         # passed to EncryptedToken from environment variable
+        super().__init__()
         self.fernet_key = Fernet(key)
 
     def process_bind_param(self, value: str | None, dialect):
         if value is None:
             return value
         else:
-            return self.fernet_key.encrypt(value.encode(encoding="utf-8"))
+            return self.fernet_key.encrypt(value.encode(encoding = "utf-8"))
     
     def process_result_value(self, value: bytes | None, dialect):
         if value is None:
             return value
         else:
-            return self.fernet_key.decrypt(value).decode(encoding="utf-8")
+            return self.fernet_key.decrypt(value).decode(encoding = "utf-8")
     
         
 
@@ -48,11 +49,13 @@ class Tokens(Base):
     id:Mapped[int] = mapped_column(primary_key = True)
 
     # encrypted token value by key stored as environment variable
-    value:Mapped[str] = mapped_column(EncryptedToken(ENCRYPTION_KEY), nullable=False)
+    value:Mapped[str] = mapped_column(EncryptedToken(ENCRYPTION_KEY), nullable = False)
 
-    expiry_date:Mapped[DateTime] = mapped_column(nullable=False)
+    expiry_date:Mapped[datetime.datetime] = mapped_column(DateTime,nullable = False)
 
-
+'''
 class Stock(Base):
     # to be implemented
     pass
+'''
+
