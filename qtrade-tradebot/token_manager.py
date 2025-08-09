582a938c04f57903e803339bae018a20de9dd378
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-08-09 16:55:35 -0600

    fixed detached token errors, added session_maker context manager, changed queries to more modern style for sqlalchemy2, refactored refresh_tokens to compliment changes to init_token, renamed init_token to check_token

--- qtrade-tradebot/token_manager.py
+++ qtrade-tradebot/token_manager.py
@@ -2,56 +2,72 @@ import requests
 import datetime
 from models import Tokens
 from env_vars import REFRESH_TOKEN
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import select
+from contextlib import contextmanager
+
+@contextmanager
+def session_manager(SessionLocal: sessionmaker):
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        print(f"Exception Occurred {e}")
+        session.rollback()
+        raise e
+    finally:
+        session.close()
+
 
 # main class for managing refresh and access tokens; automatically refresh OAuth tokens
+# interfaces with SQLAlchemy
 class TokenManager():
-    def __init__(self, session: Session):
+    def __init__(self, sessionmaker: sessionmaker):
         # session will be used to obtain refresh and access tokens
-        self.session = session
+        self.SessionLocal = sessionmaker
 
-        # self.token will be a Tokens instance
-
-        # only run at the beginning, contains starting token logic
-        self.init_token()
+        # only used for initialization
+        self.check_token()
     
 
-    def init_token(self):
-        with self.session as session:
-            # each time a new token is refreshed, replace obsolete old data
+    # checks if token exists in DB; if a token does not exist, use environment variable
+    def check_token(self):
+        with session_manager(self.SessionLocal) as session:
+            # each time a new token is refreshed token will be stored on id of 1: overwritten for each new refresh
+            
             # thus only one token should be in the database
-            # token will be stored on id of 1: overwritten for each new refresh
-            result = session.query(Tokens).filter(Tokens.id == 1).first()
-            # returns either None or Tokens object
+            result = session.scalars(select(Tokens).filter(Tokens.id == 1)).first()
+            # returns either None or a Tokens object
             
             if result is None:
                 # no token in database: grab from environment variable (likely expired)
                 # will need to check for expiry separately
-
                 from env_vars import REFRESH_TOKEN
 
-                # do not add this bootstrap token to db, refresh_tokens to get new pair
-                self.refresh_tokens(refresh_token = REFRESH_TOKEN)
-            
+                # refresh_tokens to get new pair; refresh_tokens will set self.token
+                self.refresh_tokens(session, refresh_token = REFRESH_TOKEN)
+
                 
             else:
                 self.token = result
+                # note: will become detached when session closes!
+                # will need to be reattached with session.merge when accessed in the future
 
 
     # will overwrite existing token row or add row if none exist
     # does not check for expiry logic!
-    def refresh_tokens(self, **kwargs):
-        ADDRESS = 'https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token={token}'
+    def refresh_tokens(self, session: Session, refresh_token = None):
+        # session can be passed to refresh_tokens to ensure that transactions completed in refresh_tokens remain consistent with parent function
 
-        if 'refresh_token' in kwargs:
-            # if given a starter refresh_token (from environment variable) use it
-            print(kwargs['refresh_token'])
-            result = requests.get(ADDRESS.format(token = kwargs['refresh_token']))
 
-        else:
-            # otherwise, assume that self.token is set and utilize self.token to get 
-            result = requests.get(ADDRESS.format(token = self.token.refresh_token))
+        REFRESH_ADDRESS = 'https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token={token}'
 
+        token_to_use = refresh_token or session.scalars(select(Tokens).filter(Tokens.id == 1)).first()
+
+        # query for new refresh + access token
+        result = requests.get(REFRESH_ADDRESS.format(token = token_to_use))
+            
         try:
             result.raise_for_status()
 
@@ -60,16 +76,14 @@ class TokenManager():
             raise requests.HTTPError
             # implement logic to notify user via SMS/Email later
 
-        # status is 200:
-        parsed_results = self.parse_result(result)
-        # parse_result will return a Token object to be commited to session
 
+        parsed_token = self.parse_result(result)
+        # parse_result returns a Tokens object to be commited to session
 
-        with self.session as session:
-            session.add(parsed_results)
-            session.commit()
+        session.merge(parsed_token)
+        session.commit()
         
-        self.token = parsed_results
+        self.token = parsed_token
     
 
     def parse_result(self, result) -> Tokens:
@@ -80,6 +94,7 @@ class TokenManager():
 
 
         token = Tokens(
+            id = 1,
             access_token = json_results['access_token'], 
             refresh_token = json_results['refresh_token'], 
             api_server = json_results['api_server'], 
@@ -90,19 +105,39 @@ class TokenManager():
 
     @property
     def access_token(self):
-        # TODO: implement check for expiry 
-        if self.token is None:
-            print("Error: token is not yet defined!")
-            return None
+        # every retrieval of the token should check for expiry 
+
+        with session_manager(self.SessionLocal) as session:
+            if self.token is None:
+                raise RuntimeError("Error: token is not defined!")
+            
+            session.merge(self.token)
+            if self.check_expiry():
+                print("Error: access token is expired!")
+
+                # refresh the token
+                self.refresh_tokens(session)
         
-        return self.token.access_token
+            
+        
+            return self.token.access_token 
     
+
     @property
     def refresh_token(self):
-        # not going to check refresh token expiry: access_token will be the limiting factor
-        if self.token is None:
-            print("Error: token is not yet defined!")
-            return None
+        with session_manager(self.SessionLocal) as session:
 
-        return self.token.refresh_token
-    
+            # not going to check refresh token expiry: access_token is the limiting factor
+            if self.token is None:
+                raise RuntimeError("Error: token is not yet defined!")
+            
+            session.merge(self.token)
+            return self.token.refresh_token 
+
+    def check_expiry(self):
+        # must only be called within sessions where self.token is accessible (not detached)
+        if self.token is None:
+            raise RuntimeError("Error: token is not yet defined")
+        
+        print(self.token.expiry_date) # for testing purposes
+        return datetime.datetime.now() >= self.token.expiry_date
