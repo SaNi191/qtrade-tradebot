23e1b981e080c6ed88312da9c40072a2e27521ff
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-08-14 12:40:11 -0600

    deleted authorization.py

--- qtrade-tradebot/utils/gen_key.py
+++ qtrade-tradebot/utils/gen_key.py
@@ -1,4 +1,4 @@
-# Purpose: only use once to generate encryption key to be stored in .env
+# Purpose: only used once to generate encryption key to be stored in .env
 
 from cryptography.fernet import Fernet
 
