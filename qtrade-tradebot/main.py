32f673eb3f6f70f076463fdec0c5bdce7b1dce06
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-08-15 00:12:19 -0600

    atexit notification

--- qtrade-tradebot/main.py
+++ qtrade-tradebot/main.py
@@ -1,6 +1,6 @@
 from token_manager import TokenManager
 from db import session_maker
-
+import atexit
     
 # testing
 token = TokenManager(session_maker)
@@ -8,3 +8,7 @@ token = TokenManager(session_maker)
 print(f'refresh token: {token.refresh_token}')
 print(f'access token: {token.access_token}')
 
+@atexit.register
+def exit_msg():
+    #TODO: email user that bot as quit unexpectedly/from error
+    print("Program exited!")
