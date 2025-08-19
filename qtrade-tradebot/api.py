5dec077b34e7acef96b542c77fb02109d9586fba
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-08-18 22:18:01 -0600

    QtradeAPI, may need to use streaming with websockets

--- qtrade-tradebot/api.py
+++ qtrade-tradebot/api.py
@@ -1,3 +1,5 @@
+import requests
+
 from token_manager import TokenManager
 from sqlalchemy.orm import sessionmaker
 
@@ -11,6 +13,16 @@ class QTradeAPI:
     def __init__(self, sessionmaker: sessionmaker) -> None:
         self.token = TokenManager(sessionmaker)
 
+    @property
+    def header(self):
+        # return a header for GET requests
+        header = {
+            'Authorization': f'Bearer {self.token.access_token}'
+        }
+        return header
+    
+    def get_stock(self, stock: str):
+        pass
 
 
     
