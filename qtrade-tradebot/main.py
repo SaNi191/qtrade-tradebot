3ecce1d347f25b0e7f60fdab0369a056f985b952
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-08-21 23:55:55 -0600

    added asyncio logic

--- qtrade-tradebot/main.py
+++ qtrade-tradebot/main.py
@@ -1,6 +1,10 @@
 from token_manager import TokenManager
 from db import session_maker
+from tracking.api import QTradeAPI
+from tracking.scheduler import schedule_alert, schedule_checks
+from db import session_maker
 import atexit
+import asyncio
 import logging
 logger = logging.getLogger(__name__)
 
@@ -9,7 +13,20 @@ def exit_msg():
     #TODO: email user that bot as quit unexpectedly/from error
     logger.info("Program exited!")
 
+async def main():
+    api = QTradeAPI(sessionmaker = session_maker)
+    check_task = asyncio.create_task(schedule_checks(api))
+    alert_task = asyncio.create_task(schedule_alert(api))
+
+    await asyncio.gather(check_task, alert_task)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
 # testing
+
 token = TokenManager(session_maker)
 
 logger.info(f'refresh token: {token.get_refresh_token()}')
